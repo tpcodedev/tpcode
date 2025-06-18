02bcb2e64a906923903ab5981eb3536183cc1a93
Thomas Jackson <thomas.santos@finxapp.com.br>
2025-06-18 15:24:27 -0300

    fix: remover static_site_generator: nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
     ]
   },
   app: {
-    baseURL: './',
+    baseURL: '',
     // baseURL: process.env.BASE_URL || '/',
     head: {
       title: 'TP Code - Web & Mobile Development Freelancers',
