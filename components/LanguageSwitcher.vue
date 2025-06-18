505b4a4165d1091664f15df6f4fb287b1b632075
Thomas Jackson <thomas.santos@finxapp.com.br>
2025-06-18 15:14:56 -0300

    fix: alterar a base url para ./

--- components/LanguageSwitcher.vue
+++ components/LanguageSwitcher.vue
@@ -10,7 +10,7 @@
     >
       <span class="col-start-1 row-start-1 flex items-center gap-3 pr-6">
         <span class="size-5 shrink-0 rounded-full">{{ $i18n.locale === 'pt' ? '🇧🇷' : '🇺🇸' }}</span>
-        <span class="block truncate">{{ $i18n.locale }}</span>
+        <span class="block truncate">{{ $i18n.locale }} Thomas</span>
       </span>
       <svg class="col-start-1 row-start-1 size-5 self-center justify-self-end text-gray-500 sm:size-4" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
         <path fill-rule="evenodd" d="M5.22 10.22a.75.75 0 0 1 1.06 0L8 11.94l1.72-1.72a.75.75 0 1 1 1.06 1.06l-2.25 2.25a.75.75 0 0 1-1.06 0l-2.25-2.25a.75.75 0 0 1 0-1.06ZM10.78 5.78a.75.75 0 0 1-1.06 0L8 4.06 6.28 5.78a.75.75 0 0 1-1.06-1.06l2.25-2.25a.75.75 0 0 1 1.06 0l2.25 2.25a.75.75 0 0 1 0 1.06Z" clip-rule="evenodd" />
